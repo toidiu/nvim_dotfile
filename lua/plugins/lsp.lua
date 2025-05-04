09d48e9e8a79b2de2c9f5ebbe87d4722ffdfa36b
Apoorv Kothari <akothari@cloudflare.com>
2025-05-04 11:35:54 -0700

    decently working nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,59 @@
-local root_files = {
-  '.luarc.json',
-  '.luarc.jsonc',
-  '.luacheckrc',
-  '.stylua.toml',
-  'stylua.toml',
-  'selene.toml',
-  'selene.yml',
-  '.git',
-}
+-- Configuration for LSP diagnostics
+-- vim.diagnostic.config({
+--   virtual_text = false, -- Turn off inline diagnostics
+--   signs = true,
+--   update_in_insert = false,
+--   underline = true,
+--   severity_sort = true,
+--   float = { focusable = false, style = "minimal", border = "rounded", source = "always", header = "", prefix = "" }
+-- })
+
+local base_on_attach = function(_, bufnr)
+  -- Format on save
+  -- vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format()")
+
+  local function buf_set_keymap(key, cmd)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", key, cmd, { noremap = true, silent = true })
+  end
+
+  -- vim.keymap.set("n", "gd", vim.lsp_definitions, { buffer = buffnr, desc = "vim.lsp_definitions" })
+  -- vim.keymap.set("n", "gr", vim.lsp_references, { buffer = buffnr, desc = "vim.lsp_references" })
+  -- vim.keymap.set("n", "gi", vim.lsp_implementations, { buffer = buffnr, desc = "vim.lsp_implementations" })
+
+  buf_set_keymap("gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>")
+  buf_set_keymap("gr", "<cmd>lua require('telescope.builtin').lsp_references()<CR>")
+  buf_set_keymap("gi", "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>")
+  buf_set_keymap("[g", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+  buf_set_keymap("]g", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+  -- Docs: https://neovim.io/doc/user/lsp.html#vim.lsp.buf.code_action()
+  -- quicfix
+  buf_set_keymap("gf", "<cmd>lua vim.lsp.buf.code_action({apply=true, context={only={'quickfix'}}})<CR>")
+  buf_set_keymap("gh", "<cmd>lua vim.lsp.buf.hover()<CR>")
+  buf_set_keymap("gR", "<cmd>lua vim.lsp.buf.rename()<CR>")
+
+  -- Diagnostics 
+  -- Via setloclist, setqflist
+  -- Docs: https://neovim.io/doc/user/builtin.html#setqflist()
+  -- buf_set_keymap("ge", "<cmd>lua vim.diagnostic.setqflist()<CR>")
+
+  -- Docs: https://github.com/nvim-telescope/telescope.nvim/blob/master/doc/telescope.txt
+  -- Docs: https://neovim.io/doc/user/diagnostic.html
+  -- buf_set_keymap("ge", "<cmd>lua require('telescope.builtin').diagnostics( {severity_limit=vim.diagnostic.severity.WARN} )<CR>")
+  buf_set_keymap("ge", "<cmd>lua require('telescope.builtin').diagnostics()<CR>")
+
+  -- buf_set_keymap("gh", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+  -- -- buf_set_keymap("<C-x><C-x>", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+  -- -- buf_set_keymap("gs", "<cmd>lua require('telescope.builtin').lsp_document_symbols()<CR>")
+end
+
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+-- local base_lsp = {
+--   on_attach = base_on_attach,
+--   capabilities = capabilities
+-- }
+
+-- ------------------
 
 return {
     "neovim/nvim-lspconfig",
@@ -20,8 +66,8 @@ return {
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+        -- "L3MON4D3/LuaSnip",
+        -- "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
 
@@ -42,73 +88,74 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
-                "lua_ls",
+                -- "lua_ls",
                 "rust_analyzer",
                 "gopls",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup {
+                        on_attach = base_on_attach,
                         capabilities = capabilities
                     }
                 end,
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
+                -- zls = function()
+                --     local lspconfig = require("lspconfig")
+                --     lspconfig.zls.setup({
+                --         root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                --         settings = {
+                --             zls = {
+                --                 enable_inlay_hints = true,
+                --                 enable_snippets = true,
+                --                 warn_style = true,
+                --             },
+                --         },
+                --     })
+                --     vim.g.zig_fmt_parse_errors = 0
+                --     vim.g.zig_fmt_autosave = 0
 
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                format = {
-                                    enable = true,
-                                    -- Put format options here
-                                    -- NOTE: the value should be STRING!!
-                                    defaultConfig = {
-                                        indent_style = "space",
-                                        indent_size = "2",
-                                    }
-                                },
-                            }
-                        }
-                    }
-                end,
+                -- end,
+                -- ["lua_ls"] = function()
+                --     local lspconfig = require("lspconfig")
+                --     lspconfig.lua_ls.setup {
+                --         capabilities = capabilities,
+                --         settings = {
+                --             Lua = {
+                --                 format = {
+                --                     enable = true,
+                --                     -- Put format options here
+                --                     -- NOTE: the value should be STRING!!
+                --                     defaultConfig = {
+                --                         indent_style = "space",
+                --                         indent_size = "2",
+                --                     }
+                --                 },
+                --             }
+                --         }
+                --     }
+                -- end,
             }
         })
 
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
+            -- snippet = {
+            --     expand = function(args)
+            --         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            --     end,
+            -- },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-j>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-k>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = "copilot", group_index = 2 },
+                -- { name = "copilot", group_index = 2 },
                 { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                -- { name = 'luasnip' }, -- For luasnip users.
             }, {
                 { name = 'buffer' },
             })
