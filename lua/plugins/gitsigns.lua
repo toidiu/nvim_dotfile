c3b6e510e5513c16d0b2c4acf7a905fe0db0ef8a
Apoorv Kothari <akothari@cloudflare.com>
2025-05-04 23:17:07 -0700

    gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,19 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    -- TODO: remove config = true
-    config = true,
     event = { "BufReadPre", "BufReadPost", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        -- Source: https://github.com/lewis6991/gitsigns.nvim?tab=readme-ov-file#%EF%B8%8F-installation--usage
+        signs = {
+          add          = { text = '+' },
+          change       = { text = '~' },
+          delete       = { text = '-' },
+          topdelete    = { text = '-' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+      })
+    end
   },
 }
