aad6b694dd8c71cbe944d0d403e46949ea0a042a
Apoorv Kothari <akothari@cloudflare.com>
2025-05-07 00:03:42 -0700

    cleanup

--- basic.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "lewis6991/gitsigns.nvim",
-    -- TODO: remove config = true
-    config = true,
-    event = { "BufReadPre", "BufReadPost", "BufNewFile" },
-  },
-  "ludovicchabant/vim-gutentags",
-}
