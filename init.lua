04d33943bf3617ab4a35c45eaa3eb93f46bea59f
Apoorv Kothari <akothari@cloudflare.com>
2025-05-10 21:44:54 -0700

    cleanup

--- init.lua
+++ init.lua
@@ -17,6 +17,4 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
-  install = { colorscheme = { "melange" } },
-})
+require("lazy").setup("plugins")
--- lua/plugins/vim-commentary.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "tpope/vim-commentary",
-    event = BUF_READ,
-    keys = { mode = { "n", "v", "x" } },
-    -- TODO: figure out how to lazily load vim-commentary, for some reason you need lazy = false and stuff
-    lazy = false,
-  },
-}
